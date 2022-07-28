ee6281b912307ce607fa2379b20f927aa373faa9
MGatner <mgatner@icloud.com>
2022-07-28 19:32:10 +0000

    Update DevKit

--- composer-unused.php
+++ composer-unused.php
@@ -7,12 +7,9 @@ use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
 use Webmozart\Glob\Glob;
 
-return static function (Configuration $config): Configuration {
-    return $config
-        ->addNamedFilter(NamedFilter::fromString('components/jquery'))
-        ->addNamedFilter(NamedFilter::fromString('tatter/assets'))
-        ->addNamedFilter(NamedFilter::fromString('twbs/bootstrap'))
-        ->setAdditionalFilesFor('codeigniter4/framework', [
-            ...Glob::glob(__DIR__ . '/vendor/codeigniter4/framework/system/Helpers/*.php'),
-        ]);
-};
+return static fn (Configuration $config): Configuration => $config
+    // ->addNamedFilter(NamedFilter::fromString('symfony/config'))
+    // ->addPatternFilter(PatternFilter::fromString('/symfony-.*/'))
+    ->setAdditionalFilesFor('codeigniter4/framework', [
+        ...Glob::glob(__DIR__ . '/vendor/codeigniter4/framework/system/Helpers/*.php'),
+    ]);
